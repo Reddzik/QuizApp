13642b4925cd0d88309c032d2cfb7f4ce691808c
Reddzik <mateusz.redzik@o2.pl>
2020-03-24 23:34:59 +0100

    +questions +answers +prompts

--- public/script.js
+++ public/script.js
@@ -1,11 +1,95 @@
 const question = document.querySelector("p.question");
-
+const answerButtons = document.querySelectorAll("button.answer-btn");
+const correctAnswersCountField = document.querySelector("p.correctAnswers");
+const winMessage = document.querySelector("h2.isWinMessage");
+const promptButtons = document.querySelectorAll('button.askForHelp-btn');
+const promptResult = document.querySelector("p.promptResult");
 const fillQuestionElements = (data) =>{
+    if(data.youWin === true) {
+        answerButtons.forEach(button => button.disabled = true);
+        winMessage.innerText = 'Gratulacje! Wygrałeś życie!';
+        winMessage.style.display='block';
+        return;
+    }
+    else if(data.youLose ===true){
+        answerButtons.forEach(button => button.disabled = true);
+        winMessage.innerText = 'Przegrałeś :O Co się dzieje? Co się dzieje?';
+        return;
+    }
     question.innerText = data.question;
     for (let i = 0; i < data.answers.length ; i++) {
-        const answerButton = document.querySelector(`button.answer__button-${i+1}`);
+        const answerButton = document.querySelector(`button.answer_button-${i+1}`);
         answerButton.innerText = data.answers[i];
     }
+};
+const clearPromptFiled = () =>{
+    promptResult.innerText='';
+};
+const usePrompt = (promptId)=>{
+    const promptName = choosePromptName(promptId);
+    fetch(`/prompt/${promptName}`, {
+        method:"GET",
+    }).then(res=> res.json())
+        .then(data => chooseCorrectTypeOfPrompt(promptId,data));
+};
+const choosePromptName =(promptId)=>{
+    if(Number(promptId)===0)
+        return 'askPCH';
+    else if(Number(promptId)===1)
+        return 'askStudents';
+    else if(Number(promptId)===2)
+        return 'tomaszewHelp';
+    else
+        return 0;
+};
+const chooseCorrectTypeOfPrompt = (promptId, data) =>{
+    if(Number(promptId)===0){
+        showPrompt(data);
+        return;
+    }
+    else if(Number(promptId)===1){
+        /*Pytanie do studentów*/
+    }
+    else if(Number(promptId)===2){
+        halfOnHalfPrompt(data);
+        return;
+    }
+}
+const halfOnHalfPrompt = (data)=>{
+    console.log(data.answer);
+    const answerBtn = [...answerButtons];
+    const buttonsToDelete = answerBtn.filter(button => {
+        if(button.innerText===data.answer[0] || button.innerText === data.answer[1])
+            return false;
+        else
+            return true;
+    })
+    console.log(buttonsToDelete);
+    buttonsToDelete.forEach(button=> button.style.display="none");
+
+}
+promptButtons.forEach(button => {
+    button.addEventListener('click', (e)=>{
+        const promptId = button.getAttribute("data-prompt");
+        usePrompt(promptId);
+    });
+});
+
+answerButtons.forEach((button)=>{
+    button.addEventListener('click', (event)=>{
+        event.preventDefault();
+        sendAnswer(button.getAttribute("data-answer"));
+    })
+});
+
+const showPrompt = (data) =>{
+    if(data.answer===null)
+        return;
+    promptResult.textContent=data.answer;
+};
+
+const updateCorrectAnswersCountFiled = (data) =>{
+    correctAnswersCountField.innerText = "Poprawne odpowiedzi: "+ data.goodAnswers;
 };
 
 
@@ -14,7 +98,23 @@ const showNextQuestion = () =>{
         method:'GET',
     }).then(res => res.json())
         .then(data => {
-            fillQuestionElements(data)
+            fillQuestionElements(data);
+            clearPromptFiled();
         })
 };
+
+const sendAnswer= (answer)=>{
+    fetch(`/answer/${answer}`, {
+        method:"POST",
+    }).then(res => res.json())
+        .then(data =>{
+            handleFeedbackAfterQuestion(data);
+        });
+};
+
 showNextQuestion();
+
+const handleFeedbackAfterQuestion = (data) =>{
+    updateCorrectAnswersCountFiled(data);
+    showNextQuestion();
+}
--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,11 @@
  function Game(app) {
     let  goodAnswers = 0;
+    let isGameOver =false;
     let questionToTheCrowdUsed = false;
     let callTheFriendUsed = false;
     let halfOnHalfUsed = false;
+
+    const unHelpResponse = ["Nie dzwoń do mnie więcej.", "Odczep się.", "Nie pomogę Ci więcej"];
     const dataQuestions = [
         {
             question: 'Jaka funkcja wypisuje nową linię w konsoli w języku Java',
@@ -29,7 +32,10 @@
             res.json({
                 youWin: true,
             });
-            res.end();
+        } else if(isGameOver){
+            res.json({
+                youLose: true,
+            });
         }
 
         const {question, answers} = dataQuestions[goodAnswers]; // if 0 -> 1st question
@@ -39,6 +45,64 @@
         });
         res.end();
     });
+
+    app.post('/answer/:id', (req, res)=>{
+        if(isGameOver)
+            res.json({youLose: true});
+        else if(goodAnswers===dataQuestions.length)
+            res.json({youWin:true});
+
+        const {correctAnswer} = dataQuestions[goodAnswers];
+        const isCorrect = correctAnswer===Number(req.params.id)?true:false;
+        console.log();
+        if(isCorrect)
+            goodAnswers++;
+        else
+            isGameOver=true;
+
+        res.json({
+            correct: isCorrect,
+            goodAnswers,
+        });
+    });
+
+    app.get('/prompt/askPCH', (req, res)=>{
+        const unHelpfulAnswer = unHelpResponse[Math.floor(Math.random() * unHelpResponse.length)];
+        const helpResponse = [
+            `2000 lat temu. Istaniał pewien matematyk, Giorgio Borgio Romanowski... 
+            Jak poszedł po jedzenie, bo był zamożny i jadł, to wymyślił pewną liczbę 
+            - 4f*3e^2, więc odpowiedź na Twoje pytanie to: `,
+            `Odpowiedź jest dość banalna proszę Państwa. Otóż sztuczna inteligencja, która w roku 2000 pomagała wygrać partię w szachy w Hogwarcie opracowała pewien algorytm.
+            Mianowicie... Jeśli najpierw zbijesz konia, to reszta gry przebiega w miłej atmosferze. ALE odpowiedź na to pytanie powiedział mi ostatnio fryzjer, jest to: `];
+        if (callTheFriendUsed) {
+            res.json({
+                answer : unHelpfulAnswer,
+            });
+            return;
+        }
+        const {answers, correctAnswer}= dataQuestions[goodAnswers];
+        const helpfulAnswer = `${helpResponse[Math.floor(Math.random() * helpResponse.length)]} ${answers[correctAnswer]}`;
+         callTheFriendUsed=true;
+        res.json({
+            answer: helpfulAnswer,
+        })
+    });
+
+    app.get('/prompt/tomaszewHelp', (req, res)=>{
+        if(questionToTheCrowdUsed){
+           res.json({
+               answer:unHelpResponse
+           });
+           return;
+        }
+        const {answers, correctAnswer}= dataQuestions[goodAnswers];
+        const wrongAnswers = answers.filter((s,index)=>index!==correctAnswer);
+        const randomWrongAnswer = wrongAnswers[Math.floor(Math.random()*wrongAnswers.length)];
+
+        res.json({
+            answer: [answers[correctAnswer], randomWrongAnswer],
+        });
+    })
 
 }
 module.exports= Game;
